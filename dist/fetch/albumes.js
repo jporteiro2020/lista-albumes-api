2f51ba4c73d2438e7dce3d99cfaece8c3fe0c41c
Jonatan <jonatan.porteiro@protonmail.com>
2021-09-24 00:22:44 -0300

    Elimino los archivos compilados de la carpeta dist

--- dist/fetch/albumes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fetchAlbumes = void 0;
-function fetchAlbumes(idArtista) {
-}
-exports.fetchAlbumes = fetchAlbumes;
